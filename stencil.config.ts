58998ef0936c9e33a94827762833f2a0aa3d6a16
Conan-Flexera <cmcnicholl@flexera.com>
2021-04-13 23:56:50 +0100

    Fixing config

--- stencil.config.ts
+++ stencil.config.ts
@@ -1,19 +1,16 @@
 import { Config } from '@stencil/core';
 import { sass } from '@stencil/sass';
-import { reactOutputTarget } from '@stencil/react-output-target';
 
 export const config: Config = {
   namespace: 'stencil-components',
   globalStyle: 'src/global/styles.scss',
   plugins: [sass()],
   outputTargets: [
-    reactOutputTarget({
-      componentCorePackage: 'stencil-components',
-      proxiesFile: '../stencil-components-react/src/components.ts',
-      includeDefineCustomElements: true,
-    }),
     {
       type: 'dist',
+    },
+    {
+      type: 'www',
     },
   ],
 };
